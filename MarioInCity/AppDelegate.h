825c6b8b4c797bf20aba877477a8d40118bd6ec4
mac <mac@Thinhdts-MacBook-Pro.local>
2015-07-29 21:58:11 +0700

    Initial Commit

---
+++ MarioInCity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MarioInCity
+//
+//  Created by mac on 7/29/15.
+//  Copyright (c) 2015 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MarioInCity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MarioInCity
+//
+//  Created by mac on 7/29/15.
+//  Copyright (c) 2015 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
